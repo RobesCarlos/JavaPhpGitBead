4e6ecf81168ef647553529b3b975333327e8e1b2
Unknown <robesz14@gmail.com>
2018-01-15 16:20:02 +0100

    Elso commit
    Szoveg leirasa.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="kinezetvilagos.css"> 
+</head>
+<body>
+
+<h1>Ez egy valamilyen oldal ,amit commitolni kell...</h1>
+
+<pre>Pater noster, qui es in caelis,
+sanctificetur nomen tuum,
+adveniat regnum tuum,
+fiat voluntas tua sicut in caelo et in terra.
+Panem nostrum quotidianum da nobis hodie.
+Et dimitte nobis debita nostra,
+sicut et nos dimittimus debitoribus nostris,
+et ne nos inducas in tentationem,
+sed libera nos a malo. Amen.  </pre>
+
+</body>
+</html>
